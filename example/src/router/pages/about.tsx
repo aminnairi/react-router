a119e3fd03b3cf4ed302db69b602d150a57b68a0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-03-23 21:33:16 +0100

    testing the new useLink hook

--- example/src/router/pages/about.tsx
+++ example/src/router/pages/about.tsx
@@ -2,12 +2,14 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
-import { createPage, useNavigateToPage } from "@aminnairi/react-router"
+import { createPage, useNavigateToPage, useLink } from "@aminnairi/react-router"
 import { home } from "./home";
+import { user } from "./user";
 
 export const about = createPage({
   path: "/about",
   element: function AboutPage() {
+    const Link = useLink(user);
     const navigateToHome = useNavigateToPage(home);
 
     return (
@@ -18,6 +20,9 @@ export const about = createPage({
         <Button variant="contained" sx={{ alignSelf: "center" }} onClick={navigateToHome}>
           Home
         </Button>
+        <Typography>
+          Or go to the <Link parameters={{ user: "123" }}>user#123</Link> page.
+        </Typography>
       </Stack>
     );
   }
