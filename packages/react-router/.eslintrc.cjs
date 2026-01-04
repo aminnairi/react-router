1a3f2a03dd73d9eadeeed7708473a2eef4d44aff
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-04 21:25:47 +0100

    initial commit

---
+++ packages/react-router/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'index.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: [],
+  rules: {},
+}
